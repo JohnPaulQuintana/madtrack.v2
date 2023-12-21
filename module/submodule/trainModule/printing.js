a06e5345870dd017dc9f4d3138a0d7d627af6dfd
abirmehedi77 <86663285+abirmehedi77@users.noreply.github.com>
2023-12-21 15:40:52 +0800

    updates

--- module/submodule/trainModule/printing.js
+++ module/submodule/trainModule/printing.js
@@ -6,96 +6,40 @@ module.exports = function addPrinting(manager, products) {
     "command.print.available"
   );
 
-  
+  // Function to check and convert product names to arrays
+  const convertToArrays = (item) => {
+    if (typeof item === 'string' && item.includes(' ')) {
+        console.log(item.split(' '));
+        // Convert to array if the product name has more than one word
+        return item.split(' ');
+      }
+      return [item];
+};
 
   //if they select a products
   products.forEach((product) => {
-    manager.addNamedEntityText("product", product, ["en"], [product]);
-    manager.addDocument(
-      "en",
-      `Print all the available %product% that we have?`,
-      "command.print.available.product",
-      { product: product }
-    );
-
-    // Example 1
-    manager.addDocument(
-      "en",
-      `Could you print a list of all available %product% we currently have?`,
-      "command.print.available.product",
-      { product: product }
-    );
-
-    // Example 2
-    manager.addDocument(
-      "en",
-      `Print me a list of all the %product% that are currently available.`,
-      "command.print.available.product",
-      { product: product }
-    );
-
-    // Example 3
-    manager.addDocument(
-      "en",
-      `I'd like to see a printout of all available %product%. Can you do that?`,
-      "command.print.available.product",
-      { product: product }
-    );
-
-    // Example 4
-    manager.addDocument(
-      "en",
-      `Can you print all the %product% that we currently have in stock?`,
-      "command.print.available.product",
-      { product: product }
-    );
-
-    // Example 5
-    manager.addDocument(
-      "en",
-      `I'm interested in a list of available %product%. Can you print that for me?`,
-      "command.print.available.product",
-      { product: product }
-    );
-
-    // Example 6
-    manager.addDocument(
-      "en",
-      `Print a list of all available %product%. That would be helpful.`,
-      "command.print.available.product",
-      { product: product }
-    );
-
-    // Example 7
-    manager.addDocument(
-      "en",
-      `I need a printed list of all the %product% currently available.`,
-      "command.print.available.product",
-      { product: product }
-    );
-
-    // Example 8
-    manager.addDocument(
-      "en",
-      `Could you print all the %product% that we currently have?`,
-      "command.print.available.product",
-      { product: product }
-    );
-
-    // Example 9
-    manager.addDocument(
-      "en",
-      `Print a list of all available %product% in our inventory.`,
-      "command.print.available.product",
-      { product: product }
-    );
-
-    // Example 10
-    manager.addDocument(
-      "en",
-      `Can you print a comprehensive list of available %product%?`,
-      "command.print.available.product",
-      { product: product }
-    );
+    const shortNames = convertToArrays(product);
+    // Add the full product name as a named entity
+    manager.addNamedEntityText('product', product, ['en'], [product.toLowerCase(), ...shortNames]);
+    shortNames.forEach(shortName => {
+      // manager.addNamedEntityText('product', product, ['en'], [product.toLowerCase(), shortNames])
+      manager.addDocument(
+        "en",
+        `Print all the available %product% that we have?`,
+        "command.print.available.product",
+        { product: shortName.toLowerCase() }
+      );
+  
+  
+      // Example 7
+      manager.addDocument(
+        "en",
+        `I need a printed list of all the %product% currently available.`,
+        "command.print.available.product",
+        { product: shortName.toLowerCase() }
+      );
+  
+  });
+   
   });
 };
